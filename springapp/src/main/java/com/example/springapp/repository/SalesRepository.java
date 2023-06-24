f3b57ed71b43bcf8d98cc19e2434b96026b6b2ec
SabareeshP123 <sabareeshprabhakaran@gmail.com>
2023-06-24 11:11:36 +0000

    created database

--- springapp/src/main/java/com/example/springapp/repository/SalesRepository.java
+++ springapp/src/main/java/com/example/springapp/repository/SalesRepository.java
@@ -2,7 +2,7 @@ package main.java.com.example.springapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.examly.springapp.model.Sales;
+import com.example.springapp.model.Sales;
 
 @Repository
 public interface SalesRepository extends JpaRepository<Sales,Long>{
