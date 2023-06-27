685425b9232a35887e2b8372df2295a29a2dd469
SabareeshP123 <sabareeshprabhakaran@gmail.com>
2023-06-27 11:06:47 +0000

    frontend

--- springapp/src/main/java/com/example/springapp/repository/SalesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springapp.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface SalesRepository extends JpaRepository<Sales,Long>{
-    
-}
