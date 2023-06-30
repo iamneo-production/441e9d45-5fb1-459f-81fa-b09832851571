b3097e7ec95ef34a693d3852b284f73861b0b216
mowliesh26 <mowli1947@gmail.com>
2023-06-30 15:02:25 +0000

    Barcode commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import  Barcodescannerintegeration from './component/Barcodescannerintegeration';
+import './App.css'; 
+
+const App = () => {
+  return (
+    
+    
+     <div style={{backgroundColor: "#84A7A1"}}>
+      < Barcodescannerintegeration />
+    </div>
+    
+  );
+};
+export default App;
