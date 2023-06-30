ad3b74310ef4c28160c656e93123889878f4e2a9
YudhishSharma <djyudi23sep@gmail.com>
2023-06-30 21:04:03 +0000

    Backend Commit

--- springapp/src/main/java/com/example/springapp/controller/ProductController.java
+++ springapp/src/main/java/com/example/springapp/controller/ProductController.java
@@ -1,46 +1,78 @@
 package com.example.springapp.controller;
 
-import com.example.springapp.model.Product;
-import com.example.springapp.service.ProductService;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.springapp.model.Product;
+import com.example.springapp.service.ProductService;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/api")
 public class ProductController {
 
-    private final ProductService productService;
-
     @Autowired
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+    private ProductService productService;
 
-    @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
-        List<Product> products = productService.getAllProducts();
-        return new ResponseEntity<>(products, HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
-        Product createdProduct = productService.addProduct(product);
+    @PostMapping("/products")
+    public ResponseEntity<Product> createNewProduct(@RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
         return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
     }
 
-    @PutMapping
-    public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
-        Product updatedProduct = productService.updateProduct(product);
-        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> productList = productService.getAllProduct();
+        return new ResponseEntity<>(productList, HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) {
-        productService.deleteProduct(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<String> updateProductById(@PathVariable Long id, @RequestBody Product product) {
+        Product existingProduct = productService.getProductById(id);
+        if (existingProduct != null) {
+            product.setId(existingProduct.getId());
+            productService.updateProduct(product);
+            return new ResponseEntity<>("Product with id = " + id + " updated successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Product Not Found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<String> deleteProductById(@PathVariable Long id) {
+        Product existingProduct = productService.getProductById(id);
+        if (existingProduct != null) {
+            productService.deleteProductById(id);
+            return new ResponseEntity<>("Product with id = " + id + " deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Product does not exist", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/products")
+    public ResponseEntity<String> deleteAllProduct() {
+        productService.deleteAllProduct();
+        return new ResponseEntity<>("All Products deleted successfully", HttpStatus.OK);
     }
 }
--- springapp/src/main/java/com/example/springapp/service/ProductService.java
+++ springapp/src/main/java/com/example/springapp/service/ProductService.java
@@ -1,35 +1,42 @@
 package com.example.springapp.service;
 
-import com.example.springapp.model.Product;
-import com.example.springapp.repository.ProductRepository;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.example.springapp.model.Product;
+import com.example.springapp.repository.ProductRepository;
 
 @Service
 public class ProductService {
 
-    private final ProductRepository productRepository;
-
     @Autowired
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    private ProductRepository productRepository;
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
     }
 
-    public List<Product> getAllProducts() {
+    public List<Product> getAllProduct() {
         return productRepository.findAll();
     }
 
-    public Product addProduct(Product product) {
-        return productRepository.save(product);
+    public Product getProductById(Long id) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        return optionalProduct.orElse(null);
     }
 
-    public Product updateProduct(Product product) {
-        return productRepository.save(product);
+    public void updateProduct(Product product) {
+        productRepository.save(product);
     }
 
-    public void deleteProduct(Long id) {
+    public void deleteProductById(Long id) {
         productRepository.deleteById(id);
+    }
+
+    public void deleteAllProduct() {
+        productRepository.deleteAll();
     }
 }
