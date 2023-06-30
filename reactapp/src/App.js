ed8e7a1a2fde771ceed5e2db4a747568d9e84a07
mowliesh26 <mowli1947@gmail.com>
2023-06-30 16:05:13 +0000

    Third commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import  Barcodescannerintegeration from './component/Barcodescannerintegeration';
 import './App.css'; 
-
+import Home from './components/Home';
 const App = () => {
   return (
     
