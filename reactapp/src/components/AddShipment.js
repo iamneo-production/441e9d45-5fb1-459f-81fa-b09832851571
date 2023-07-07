373c0c03cf787d063c0b512dd8d6561d93b8f4a2
dharshini01 <priyasenthil713@gmail.com>
2023-07-07 10:01:26 +0000

    Update

--- reactapp/src/components/AddShipment.js
+++ reactapp/src/components/AddShipment.js
@@ -21,7 +21,7 @@ export default function AddShipment() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await axios.post("http://localhost:8081/api/post", user);
+    await axios.post("https://8080-aabbafaeecebdfaddeebcaddaceaeaadbdbabf.project.examly.io/api/post", user);
     navigate("/shipment");
   };
 
--- reactapp/src/components/ViewShipment.js
+++ reactapp/src/components/ViewShipment.js
@@ -17,7 +17,7 @@ export default function ViewShipment() {
   }, []);
 
   const loadUser = async () => {
-    const result = await axios.get(`http://localhost:8081/api/get/${id}`);
+    const result = await axios.get(`https://8080-aabbafaeecebdfaddeebcaddaceaeaadbdbabf.project.examly.io/api/get/${id}`);
     setUser(result.data);
   };
 
