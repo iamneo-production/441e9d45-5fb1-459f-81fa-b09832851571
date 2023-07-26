6afde4a3082b6cbf6dc31f837913925047d2a6c0
YudhishSharma <djyudi23sep@gmail.com>
2023-07-26 03:47:27 +0000

    testing

--- springapp/src/main/java/com/example/springapp/repository/InventoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springapp.model.Inventory;
-
-public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
-}
