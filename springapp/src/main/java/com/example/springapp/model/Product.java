312f8acd18163d87f910dbe56a782bc15b2ff438
praveensiddaa <mohandurgapraveensidda@gmail.com>
2023-07-30 05:23:15 +0000

    firstpush

--- springapp/src/main/java/com/example/springapp/model/Product.java
+++ springapp/src/main/java/com/example/springapp/model/Product.java
@@ -8,12 +8,17 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name="name")
     private String name;
+    @Column(name="description")
     private String description;
+    @Column(name="price")
     private double price;
+    @Column(name="quantity")
     private long quantity;
+    @Column(name="location")
     private String location;
+    @Column(name="barcode")
     private String barcode;
 
     public Product() {
